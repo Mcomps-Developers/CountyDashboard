5bb35ff9032fd569046619c3df0b50c7b68f5740
petermuremi351 <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-18 01:23:51 +0300

    Add event category

--- app/Livewire/Events/AddEventCategory.php
+++ app/Livewire/Events/AddEventCategory.php
@@ -2,10 +2,63 @@
 
 namespace App\Livewire\Events;
 
+use App\Models\Event;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 class AddEventCategory extends Component
 {
+    public $name;
+    public $rules = [
+        'name' => 'required|unique:event_categories,name',
+    ];
+    public function updated($fields)
+    {
+        $this->validateOnly($fields);
+    }
+
+    public function addCategory()
+    {
+        $this->validate();
+        try {
+            $category = new Event();
+            $category->name = $this->name;
+            $category->slug = Str::slug($this->name, '-');
+            $category->save();
+            $this->reset();
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->success('Category Created');
+
+        } catch (\Throwable $th) {
+            Log::error('An unexpected error occurred.', [
+                'error_message' => $th->getMessage(),
+                'file' => $th->getFile(),
+                'line' => $th->getLine(),
+                'stack_trace' => $th->getTraceAsString()
+            ]);
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->error('Error occurred. Try later');
+            return redirect(request()->header('Referer'));
+        } catch (\Exception $ex) {
+            Log::warning('An exception occurred.', [
+                'error_message' => $ex->getMessage(),
+                'file' => $ex->getFile(),
+                'line' => $ex->getLine(),
+                'stack_trace' => $ex->getTraceAsString()
+            ]);
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->error('Error occurred. Try later');
+            return redirect(request()->header('Referer'));
+        }
+
+    }
     public function render()
     {
         return view('livewire.events.add-event-category')->layout('layouts.app');
--- resources/views/livewire/events/add-event-category.blade.php
+++ resources/views/livewire/events/add-event-category.blade.php
@@ -1,3 +1,60 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+<div class="content container-fluid">
+    @section('title')
+        Add Events Category
+    @endsection
+    <div class="page-header">
+        <div class="row align-items-center">
+            <div class="col">
+                <h3 class="page-title">Event</h3>
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item active">Add Category</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                    <form>
+                        <div class="row">
+                            <div class="col-12">
+                                <h5 class="form-title"><span>Add Event Category</span></h5>
+                            </div>
+                            <form wire:submit.prevent='addCategory'>
+                                <div class="col-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label>Category Name</label>
+                                        <input type="text" class="form-control" placeholder="Category Name"
+                                            wire:model.live='name'>
+                                        @error('name')
+                                            <p class="text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-12" wire:offline>
+                                    <p class="text-danger">You are offline</p>
+                                </div>
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-primary" wire:offline.attr="disabled"
+                                        wire:click.prevent='addCategory'>
+                                        <span wire:loading>Processing...</span>
+                                        <span wire:loading.remove>Submit</span>
+                                    </button>
+                                </div>
+
+                                <style>
+                                    .btn:disabled {
+                                        opacity: 0.6;
+                                        cursor: not-allowed;
+                                    }
+                                </style>
+                            </form>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
