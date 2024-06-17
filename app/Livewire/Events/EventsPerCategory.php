b1334409bb9fa13567e7852c207575a069069da2
petermuremi351 <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-18 01:56:58 +0300

    Display events per category

--- app/Livewire/Events/EventsPerCategory.php
+++ app/Livewire/Events/EventsPerCategory.php
@@ -2,13 +2,19 @@
 
 namespace App\Livewire\Events;
 
+use App\Models\Event;
+use App\Models\EventCategory;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class EventsPerCategory extends Component
 {
+    use WithPagination;
     public $slug;
     public function render()
     {
-        return view('livewire.events.events-per-category')->layout('layouts.app');
+        $category = EventCategory::where('slug', $this->slug)->first();
+        $events = Event::orderByDesc('created_at')->where('category_id', $category->id)->paginate(12);
+        return view('livewire.events.events-per-category', ['events' => $events])->layout('layouts.app');
     }
 }
