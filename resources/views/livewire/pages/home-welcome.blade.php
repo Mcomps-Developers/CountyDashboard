6ad4a66f24ae5d4d4098e68db0381ab95fdbfef4
petermuremi351 <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-19 11:48:06 +0300

    Update home-welcome.blade.php

--- resources/views/livewire/pages/home-welcome.blade.php
+++ resources/views/livewire/pages/home-welcome.blade.php
@@ -82,12 +82,12 @@
                             </div>
                             @if ($this->photo)
                                 <div class="col-12 col-sm-4">
-                                    <img src="{{ $photo->temporaryUrl() }}" width="100" height="150">
+                                    <img src="{{ $photo->temporaryUrl() }}" width="100" height="200">
                                 </div>
                             @else
                                 <div class="col-12 col-sm-4">
                                     <img src="{{ asset('assets/img/governors') }}/{{ $currentPhoto }}" width="100"
-                                        height="150">
+                                        height="200">
                                 </div>
                             @endif
                             <div class="col-12">
