f7792f2a87906cfbed96ad5bb1a3cacbadafdd4f
petermuremi351 <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-19 15:32:49 +0300

    Update AddSlider.php

--- app/Livewire/Pages/AddSlider.php
+++ app/Livewire/Pages/AddSlider.php
@@ -25,6 +25,7 @@ class AddSlider extends Component
         'start_date' => 'required|date',
         'end_date' => 'required|date',
         'image' => 'mimes:png,jpg,jpeg|max:5120',
+        'status'=>'required',
     ];
 
     public function updated($fields)
