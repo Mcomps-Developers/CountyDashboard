ed42c7daf42fc370b68862b3ad9d3abd6a75e6b3
petermuremi351 <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-19 15:31:53 +0300

    Update AddSlider.php
    Higher resolution images

--- app/Livewire/Pages/AddSlider.php
+++ app/Livewire/Pages/AddSlider.php
@@ -62,7 +62,7 @@ class AddSlider extends Component
             $slider->reference = $this->generateUniqueReference(Slider::class, 'reference', 5);
             if ($this->image) {
                 $photoName = Carbon::now()->addMinutes(2)->timestamp . '.' . $this->photo->extension();
-                $resizedImage = Image::read($this->photo->getRealPath())->resize(4800, 3200);
+                $resizedImage = Image::read($this->photo->getRealPath())->resize(5760, 3240);
                 $destinationPath = base_path('assets/img/sliders');
                 $resizedImage->save($destinationPath . '/' . $photoName);
                 $slider->image = $photoName;
