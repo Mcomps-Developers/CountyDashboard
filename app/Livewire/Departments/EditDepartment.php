33835b20c5c0ab3f6cd839cc87b2992cbcc37142
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-22 12:35:53 +0300

    Update EditDepartment.php

--- app/Livewire/Departments/EditDepartment.php
+++ app/Livewire/Departments/EditDepartment.php
@@ -39,13 +39,11 @@ class EditDepartment extends Component
         $this->description = $department->description;
         $this->title = $department->title;
         $this->chief_officer_name = $department->chief_officer_name;
-        $this->chief_officer_photo = $department->chief_officer_photo;
         $this->chief_officer_date_of_birth = $department->chief_officer_date_of_birth;
         $this->chief_officer_department_phone = $department->chief_officer_department_phone;
         $this->chief_officer_department_email = $department->chief_officer_department_email;
         $this->about_chief_officer = $department->about_chief_officer;
         $this->cecm_name = $department->cecm_name;
-        $this->cecm_photo = $department->cecm_photo;
         $this->cecm_date_of_birth = $department->cecm_date_of_birth;
         $this->cecm_department_phone = $department->cecm_department_phone;
         $this->cecm_department_email = $department->cecm_department_email;
