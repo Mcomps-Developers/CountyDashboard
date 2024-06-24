c21d9cb059c94647abcf437858dee95fd85ced02
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-24 16:45:04 +0300

    Edit Ward

--- app/Livewire/Wards/EditShowWard.php
+++ app/Livewire/Wards/EditShowWard.php
@@ -2,12 +2,87 @@
 
 namespace App\Livewire\Wards;
 
+use App\Models\Subcounty;
+use App\Models\Ward;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class EditShowWard extends Component
 {
+    public $constituency;
+    public $ward_id;
+    public $name;
+    public $short_description;
+    public $mca_name;
+    public $website_url;
+    public $subCountyName;
+
+    public function mount()
+    {
+        $ward = Ward::findOrFail($this->ward_id);
+        $this->constituency = $ward->subcounty_id;
+        $this->name = $ward->name;
+        $this->short_description = $ward->short_description;
+        $this->mca_name = $ward->mca_name;
+        $this->website_url = $ward->website_url;
+    }
+
+    public $rules = [
+        'name' => 'required|unique:wards,name',
+        'short_description' => 'required|string',
+        'mca_name' => 'nullable|string|max:60',
+        'website_url' => 'nullable|url',
+    ];
+    public function updated($fields)
+    {
+        $this->validateOnly($fields);
+    }
+
+    public function saveChanges()
+    {
+        $this->validate();
+        try {
+            $ward = Ward::findOrFail($this->ward_id);
+            $ward->subcounty_id = $this->constituency;
+            $ward->name = $this->name;
+            $ward->mca_name = $this->mca_name;
+            $ward->short_description = $this->short_description;
+            $ward->website = $this->website_url;
+            $ward->save();
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->success('Changes Saved');
+            return redirect(request()->header('Referer'));
+        } catch (\Throwable $th) {
+            Log::error('An unexpected error occurred.', [
+                'error_message' => $th->getMessage(),
+                'file' => $th->getFile(),
+                'line' => $th->getLine(),
+                'stack_trace' => $th->getTraceAsString()
+            ]);
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->error('Error occurred. Try later');
+            return redirect(request()->header('Referer'));
+        } catch (\Exception $ex) {
+            Log::warning('An exception occurred.', [
+                'error_message' => $ex->getMessage(),
+                'file' => $ex->getFile(),
+                'line' => $ex->getLine(),
+                'stack_trace' => $ex->getTraceAsString()
+            ]);
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->error('Error occurred. Try later');
+            return redirect(request()->header('Referer'));
+        }
+    }
     public function render()
     {
-        return view('livewire.wards.edit-show-ward');
+        $subCounties = Subcounty::orderBy('name')->get();
+        return view('livewire.wards.edit-show-ward', ['subCounties' => $subCounties])->layout('layouts.app');
     }
 }
--- resources/views/livewire/wards/edit-show-ward.blade.php
+++ resources/views/livewire/wards/edit-show-ward.blade.php
@@ -1,3 +1,101 @@
-<div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+<div class="content container-fluid">
+    @section('title')
+    Edit Ward
+    @endsection
+    <div class="row">
+        <div class="col-xl-12">
+
+            <div class="page-header">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h3 class="page-title">Edit Ward</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <form wire:submit.prevent="saveChanges">
+                    <div class="card-body">
+                        <div class="bank-inner-details">
+                            <div class="row">
+                                <div class="col-lg-3 col-md-12">
+                                    <div class="form-group">
+                                        <label>Ward Name<span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" wire:model.live='name'>
+                                        @error('name')
+                                        <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-12">
+                                    <div class="form-group">
+                                        <label>MCA Name</label>
+                                        <input type="text" class="form-control" wire:model.live='mca_name'>
+                                        @error('mca_name')
+                                        <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-12">
+                                    <div class="form-group">
+                                        <label>Constituency</label>
+                                        <select class="form-control" wire:model.live='constituency'>
+                                            @foreach ($subCounties as $item)
+                                            <option value="{{$item->id}}">{{$item->name}}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('constituency')
+                                        <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-12">
+                                    <div class="form-group">
+                                        <label>Website</label>
+                                        <input type="url" class="form-control" wire:model.live='website_url'>
+                                        @error('website_url')
+                                        <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12">
+                                    <div class="form-group" wire:ignore>
+                                        <label>Short Description</label>
+                                        <textarea id="short_description" class="form-control"
+                                            wire:model.live='short_description' columns="2" rows="4"></textarea>
+                                    </div>
+                                    @error('short_description')
+                                    <p class="text text-danger">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pt-0 blog-categories-btn">
+                        <div class="bank-details-btn ">
+                            <button type="submit" class="btn bank-cancel-btn me-2">Save Changes</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
+@script
+<script>
+    $(function() {
+            tinymce.init({
+                selector: '#short_description',
+                plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+                toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+                setup: function(editor) {
+                    editor.on('Change', function(e) {
+                        tinymce.triggerSave();
+                        var sd_data = $('#short_description').val();
+                        @this.set('short_description', sd_data);
+                    });
+                }
+            });
+        });
+</script>
+@endscript
