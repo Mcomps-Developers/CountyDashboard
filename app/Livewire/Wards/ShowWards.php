0394e81e21992e8e24d767de28af763df944fa36
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-24 15:46:38 +0300

    Show Wards

--- app/Livewire/Wards/ShowWards.php
+++ app/Livewire/Wards/ShowWards.php
@@ -2,12 +2,56 @@
 
 namespace App\Livewire\Wards;
 
+use App\Models\Subcounty;
+use App\Models\Ward;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class ShowWards extends Component
 {
+    public $subCounty_id;
+    public $subCountyName;
+    public function mount()
+    {
+        $subCounty = Subcounty::findOrFail($this->subCounty_id);
+        $this->subCountyName = $subCounty->name;
+    }
+    public function deleteWard($rowID)
+    {
+        try {
+            $ward = Ward::findOrFail($rowID);
+            $ward->delete();
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->success('Ward Deleted');
+        } catch (\Throwable $th) {
+            Log::error('An unexpected error occurred.', [
+                'error_message' => $th->getMessage(),
+                'file' => $th->getFile(),
+                'line' => $th->getLine(),
+                'stack_trace' => $th->getTraceAsString()
+            ]);
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->error('Error occurred. Try later');
+        } catch (\Exception $ex) {
+            Log::warning('An exception occurred.', [
+                'error_message' => $ex->getMessage(),
+                'file' => $ex->getFile(),
+                'line' => $ex->getLine(),
+                'stack_trace' => $ex->getTraceAsString()
+            ]);
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->error('Error occurred. Try later');
+        }
+    }
     public function render()
     {
-        return view('livewire.wards.show-wards');
+        $wards = Ward::orderBy('name')->where('subcounty_id', $this->subCounty_id)->get();
+        return view('livewire.wards.show-wards', ['wards' => $wards])->layout('layouts.app');
     }
 }
--- resources/views/livewire/wards/show-wards.blade.php
+++ resources/views/livewire/wards/show-wards.blade.php
@@ -1,3 +1,76 @@
-<div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+<div class="content container-fluid">
+    @section('title')
+    Wards in {{$subCountyName}}
+    @endsection
+    <div class="page-header">
+        <div class="row align-items-center">
+            <div class="col">
+                <h3 class="page-title">Administration</h3>
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item active">Wards in {{$subCountyName}}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card card-table">
+                <div class="card-body">
+
+                    <div class="page-header">
+                        <div class="row align-items-center">
+                            <div class="col">
+                                <h3 class="page-title">Wards in {{$subCountyName}}</h3>
+                            </div>
+                            <div class="col-auto text-end float-end ms-auto download-grp">
+                                <a href="javascript:void(0);" class="btn btn-outline-primary me-2"><i
+                                        class="fas fa-download"></i>
+                                    Download</a>
+                                <a href="{{ route('ward.add',['subCounty_id'=>$subCounty_id]) }}"
+                                    class="btn btn-primary"><i class="fas fa-plus"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="table-responsive">
+                        <table
+                            class="table mb-0 border-0 star-student table-hover table-center datatable table-striped">
+                            <thead class="student-thread">
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Constituency</th>
+                                    <th>Created Date</th>
+                                    <th>Updated Date</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($subcounties as $item)
+                                <tr>
+                                    <td>
+                                        <h2>
+                                            <a>{{ $item->name }}</a>
+                                        </h2>
+                                    </td>
+                                    <td>{{ $item->subCounty->name }}</td>
+                                    <td>{{ date('d M Y h:iA', strtotime($item->created_at)) }}</td>
+                                    <td>{{ date('d M Y h:iA', strtotime($item->updated_at)) }}</td>
+                                    <td>
+                                        <a href="{{route('ward.edit',['ward_id'=>$item->id])}}"><i
+                                                class="fa fa-edit"></i></a>
+                                        <a href="javascript:void(0);" wire:target='deleteWard({{ $item->id }})'
+                                            wire:click.prevent='deleteWard({{ $item->id }})'
+                                            wire:confirm='Are you sure you want to delete?'><i
+                                                class="fa fa-trash text-danger" style="padding-left: 6px;"></i></a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
