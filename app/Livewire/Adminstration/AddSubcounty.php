1f27e7e783c686ba2a9c2f2352368f25a7567f5e
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-24 14:11:47 +0300

    Add SubCounty

--- app/Livewire/Adminstration/AddSubcounty.php
+++ app/Livewire/Adminstration/AddSubcounty.php
@@ -2,12 +2,70 @@
 
 namespace App\Livewire\Adminstration;
 
+use App\Models\Subcounty;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class AddSubcounty extends Component
 {
+    public $name;
+    public $description;
+    public $MPName;
+    public $website_url;
+    public $rules = [
+        'name' => 'required|unique:subcounties,name',
+        'short_description' => 'nullable|string',
+        'MPName' => 'required',
+        'website_url' => 'nullable|url',
+    ];
+    public function updated($fields)
+    {
+        $this->validateOnly($fields);
+    }
+
+    public function saveChanges()
+    {
+        $this->validate();
+        try {
+            $subcounty = new Subcounty();
+            $subcounty->name = $this->name;
+            $subcounty->mp_name = $this->MPName;
+            $subcounty->short_description = $this->short_description;
+            $subcounty->website_url = $this->website_url;
+            $subcounty->save();
+            $this->reset();
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->success('Constituency Created');
+        } catch (\Throwable $th) {
+            Log::error('An unexpected error occurred.', [
+                'error_message' => $th->getMessage(),
+                'file' => $th->getFile(),
+                'line' => $th->getLine(),
+                'stack_trace' => $th->getTraceAsString()
+            ]);
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->error('Error occurred. Try later');
+            return redirect(request()->header('Referer'));
+        } catch (\Exception $ex) {
+            Log::warning('An exception occurred.', [
+                'error_message' => $ex->getMessage(),
+                'file' => $ex->getFile(),
+                'line' => $ex->getLine(),
+                'stack_trace' => $ex->getTraceAsString()
+            ]);
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->error('Error occurred. Try later');
+            return redirect(request()->header('Referer'));
+        }
+    }
     public function render()
     {
-        return view('livewire.adminstration.add-subcounty');
+        return view('livewire.adminstration.add-subcounty')->layout('layouts.app');
     }
 }
--- resources/views/livewire/adminstration/add-subcounty.blade.php
+++ resources/views/livewire/adminstration/add-subcounty.blade.php
@@ -1,3 +1,109 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<div class="content container-fluid">
+    @section('title')
+    New Constituency
+    @endsection
+    <div class="row">
+        <div class="col-xl-12">
+
+            <div class="page-header">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h3 class="page-title">New Constituency</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <form wire:submit.prevent="saveChanges">
+                    <div class="card-body">
+                        <div class="bank-inner-details">
+                            <div class="row">
+                                <div class="col-lg-4 col-md-12">
+                                    <div class="form-group">
+                                        <label>Name<span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" wire:model.live='name'>
+                                        @error('name')
+                                        <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-12">
+                                    <div class="form-group">
+                                        <label>MP Name<span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" wire:model.live='MPName'>
+                                        @error('MPName')
+                                        <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-12">
+                                    <div class="form-group">
+                                        <label>Website<span class="text-danger">*</span></label>
+                                        <input type="url" class="form-control" wire:model.live='website_url'>
+                                        @error('website_url')
+                                        <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12">
+                                    <div class="form-group" wire:ignore>
+                                        <label>Short Description</label>
+                                        <textarea id="short_description" class="form-control"
+                                            wire:model.live='short_description' columns="2" rows="4"></textarea>
+                                    </div>
+                                    @error('short_description')
+                                    <p class="text text-danger">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                                <div class="col-lg-6 col-md-6">
+                                    <div class="form-group">
+                                        <label>Tags</span></label>
+                                        <input type="text" class="form-control"
+                                            placeholder="Seperated by comma eg. speech, news, publication"
+                                            wire:model.live='tags'>
+                                        @error('tags')
+                                        <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6">
+                                    <div class="form-group">
+                                        <label>Scheduled on</span></label>
+                                        <input type="date" class="form-control" wire:model.live='publishing_date'>
+                                        @error('publishing_date')
+                                        <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="pt-0 blog-categories-btn">
+                        <div class="bank-details-btn ">
+                            <button type="submit" class="btn bank-cancel-btn me-2">Save Changes</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
+@script
+<script>
+    $(function() {
+            tinymce.init({
+                selector: '#short_description',
+                plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+                toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+                setup: function(editor) {
+                    editor.on('Change', function(e) {
+                        tinymce.triggerSave();
+                        var sd_data = $('#short_description').val();
+                        @this.set('short_description', sd_data);
+                    });
+                }
+            });
+        });
+</script>
+@endscript
