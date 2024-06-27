65257aab5c3cd86750292543872883eb70a8a592
petermuremi351 <mcompsnetwork@gmail.com>
2024-06-27 17:11:33 +0300

    Add Project

--- app/Livewire/Projects/AddProject.php
+++ app/Livewire/Projects/AddProject.php
@@ -2,12 +2,104 @@
 
 namespace App\Livewire\Projects;
 
+use App\Models\Department;
+use App\Models\Project;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
+use Illuminate\Support\Str;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 
 class AddProject extends Component
 {
+    use WithFileUploads;
+    private $slug;
+    public $description;
+    public $title;
+    public $cover_image;
+    public $project_date;
+    public $category_name;
+    public $location;
+    public $department;
+    public $images;
+    protected $rules = [
+        'description' => 'required|string',
+        'title' => 'required',
+        'cover_image' => 'nullable|mimes:png,jpg,jpeg|max:5120',
+        'project_date' => 'nullable|date',
+    ];
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields);
+    }
+
+    private function generateUniqueReference($model, $column, $length = 5)
+    {
+        do {
+            $reference = Str::random($length);
+            $exists = $model::where($column, $reference)->exists();
+        } while ($exists);
+
+        return $reference;
+    }
+    public function saveChanges()
+    {
+        $this->validate();
+        if ($this->photo) {
+            $this->validate([
+                'photo' => 'mimes:png,jpg,jpeg|max:5120',
+            ]);
+        }
+        try {
+            $project = new Project();
+            $project->title = $this->title;
+            $project->description = $this->description;
+            $project->slug = Str::slug($this->title, '-');
+            $project->category_name = $this->category_name;
+            $project->department_id = $this->department;
+            $project->reference = $this->generateUniqueReference(Project::class, 'reference', 5);
+            $project->created_at = empty($this->project_date) ? Carbon::now() : $this->project_date;
+            if ($this->cover_image) {
+                $photoName = Carbon::now()->addMinutes(2)->timestamp . '.' . $this->cover_image->extension();
+                $this->cover_image->storeAs('assets/img/projects/covers', $photoName);
+                $project->cover_image = $photoName;
+            }
+            $project->save();
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->success('Created successfully.');
+            return redirect(request()->header('Referer'));
+        } catch (\Throwable $th) {
+            Log::error('An unexpected error occurred.', [
+                'error_message' => $th->getMessage(),
+                'file' => $th->getFile(),
+                'line' => $th->getLine(),
+                'stack_trace' => $th->getTraceAsString()
+            ]);
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->error('Error occurred. Try later');
+            return redirect(request()->header('Referer'));
+        } catch (\Exception $ex) {
+            Log::warning('An exception occurred.', [
+                'error_message' => $ex->getMessage(),
+                'file' => $ex->getFile(),
+                'line' => $ex->getLine(),
+                'stack_trace' => $ex->getTraceAsString()
+            ]);
+            notyf()
+                ->position('x', 'right')
+                ->position('y', 'top')
+                ->error('Error occurred. Try later');
+            return redirect(request()->header('Referer'));
+        }
+    }
     public function render()
     {
-        return view('livewire.projects.add-project');
+        $departments = Department::orderBy('name')->get();
+        return view('livewire.projects.add-project', ['departments' => $departments])->layout('layouts.app');
     }
 }
--- resources/views/livewire/projects/add-project.blade.php
+++ resources/views/livewire/projects/add-project.blade.php
@@ -1,3 +1,150 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div class="content container-fluid">
+    @section('title')
+        Add Project
+    @endsection
+    <div class="row">
+        <div class="col-xl-12">
+
+            <div class="page-header">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h3 class="page-title">Add Project</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <form wire:submit.prevent="saveChanges">
+                    <div class="card-body">
+                        <div class="bank-inner-details">
+
+                            <div class="row">
+                                <div class="col-lg-12 col-md-12">
+                                    <div class="form-group">
+                                        <label>Project Title<span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" wire:model.live='title'>
+                                        @error('title')
+                                            <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-12">
+                                    <div class="form-group">
+                                        <label>Department <span class="text-danger">Optional</span></label>
+                                        <select name="" id="" class="form-control"
+                                            wire:model='department'>
+                                            <option>--</option>
+                                            @foreach ($departments as $item)
+                                                <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('department')
+                                            <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-12">
+                                    <div class="form-group">
+                                        <label>Category <span class="text-danger">Optional</span></label>
+                                        <input type="text" class="form-control" wire:model.live='category_name'>
+                                        @error('category_name')
+                                            <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-12">
+                                    <div class="form-group">
+                                        <div class="change-photo-btn">
+                                            <div>
+                                                @if ($photo)
+                                                    <p>Change Image</p>
+                                                @else
+                                                    <p>Upload Image</p>
+                                                @endif
+                                                <small>Will automatically be resized to 1200x800 pixels.</small>
+                                                <p wire:loading wire:target='photo'>Uploading...</p>
+                                            </div>
+                                            <input type="file" class="upload" wire:model.live='photo'>
+                                        </div>
+                                        @error('photo')
+                                            <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                @if ($photo)
+                                    <div class="col-lg-6 col-md-12">
+                                        <div class="form-group">
+                                            <div class="change-photo-btn">
+                                                <div>
+                                                    @if ($photo)
+                                                        <img src="{{ $photo->temporaryUrl() }}" width="200"
+                                                            height="150" alt="">
+                                                    @else
+                                                        <p>No Image has been uploaded</p>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endif
+                                <div class="col-lg-12 col-md-12">
+                                    <div class="form-group" wire:ignore>
+                                        <label>Speech Content</label>
+                                        <textarea id="content" class="form-control" wire:model.live='content' columns="2" rows="4"></textarea>
+                                    </div>
+                                    @error('content')
+                                        <p class="text text-danger">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                                <div class="col-lg-6 col-md-6">
+                                    <div class="form-group">
+                                        <label>Tags</span></label>
+                                        <input type="text" class="form-control"
+                                            placeholder="Seperated by comma eg. speech, news, publication"
+                                            wire:model.live='tags'>
+                                        @error('tags')
+                                            <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6">
+                                    <div class="form-group">
+                                        <label>Scheduled on</span></label>
+                                        <input type="date" class="form-control" wire:model.live='publishing_date'>
+                                        @error('publishing_date')
+                                            <p class="text text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="pt-0 blog-categories-btn">
+                        <div class="bank-details-btn ">
+                            <button type="submit" class="btn bank-cancel-btn me-2">Save Changes</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
+@script
+    <script>
+        $(function() {
+            tinymce.init({
+                selector: '#content',
+                plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+                toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+                setup: function(editor) {
+                    editor.on('Change', function(e) {
+                        tinymce.triggerSave();
+                        var sd_data = $('#content').val();
+                        @this.set('content', sd_data);
+                    });
+                }
+            });
+        });
+    </script>
+@endscript
