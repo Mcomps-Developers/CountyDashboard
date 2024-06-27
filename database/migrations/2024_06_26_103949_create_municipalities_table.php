595e99a6abee6016e9e8cb27415e69f322ccf9e8
petermuremi351 <mcompsnetwork@gmail.com>
2024-06-27 16:27:37 +0300

    Update 2024_06_26_103949_create_municipalities_table.php

--- database/migrations/2024_06_26_103949_create_municipalities_table.php
+++ database/migrations/2024_06_26_103949_create_municipalities_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
         Schema::table('municipalities', function (Blueprint $table) {
             $table->string('slug')->unique();
             $table->longText('content')->default('Coming soon!');
-            $table->softDeletes();
         });
     }
 
