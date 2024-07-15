6c4a5b5575a7f3a5d26401245bb6996ef76fb6ee
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-07-15 14:26:14 +0300

    Update console.php
    Clean, backup, monitor backup health

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('backup:clean')->daily()->at('01:00');
+Schedule::command('backup:run')->daily()->at('01:30');
+Schedule::command('backup:monitor')->daily()->at('03:10');
